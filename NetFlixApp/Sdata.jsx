7562ca92b3e2ab895541b9d1cb666bd8b0bb02e6
Yogesh Singh rawat <rawaty20929@gmail.com>
2021-05-24 12:32:29 +0530

    Add files via upload

---
+++ NetFlixApp/Sdata.jsx
@@ -0,0 +1,33 @@
+const Sdata=[{
+    sname:"Dark",
+    imgsrc:"https://picsum.photos/200/300",
+    title:"A website Original series",
+    links:"https://picsum.photos/",
+
+},
+{
+    sname:"extra curriculam",
+    imgsrc:"https://picsum.photos/250/300",
+    title:"A website Original series",
+    links:"https://picsum.photos/",
+},
+{
+    sname:"Stranger things",
+    imgsrc:"https://picsum.photos/208/300",
+    title:"A website Original series",
+    links:"https://picsum.photos/",
+},
+{
+    sname:"The Vampire Diries",
+    imgsrc:"https://picsum.photos/230/300",
+    title:"A website Original series",
+    links:"https://picsum.photos/",
+},
+{
+    sname:"The Vampire Diries",
+    imgsrc:"https://picsum.photos/230/300",
+    title:"A website Original series",
+    links:"https://picsum.photos/",
+}
+]
+ export default Sdata;
--- NetFlixApp/index.js
+++ NetFlixApp/index.js
@@ -1,1 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Card from "./Cards";
+import "./index.css";
+import Sdata from "./Sdata";
+//console.log(Sdata[0].sname);
 
+// function ncard(val)
+// {
+//     console.log(val);
+// return(
+//     <Card 
+//     imgsrc={val.imgsrc} 
+//     title={val.title}
+//     sname={val.sname} 
+//     link={val.links}
+
+//     />
+// );
+// }
+ReactDOM.render(
+    <>
+    <h1 className="heading_style">Top 4 Netflix series..</h1>
+   
+    {
+        Sdata.map((val,index)=>
+    {
+        console.log(index);
+        return(
+    <Card 
+    imgsrc={val.imgsrc} 
+    title={val.title}
+    sname={val.sname} 
+    link={val.links}
+
+    />);})} 
+    </>,
+    document.getElementById("root")
+);
